43ac3ca193f64c91225bd2fbd72f4ef8dc69abb1
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2024-11-21 20:36:22 +0100

    Added saving users to DB on register

---
+++ app/api/auth/success/route.ts
@@ -0,0 +1,29 @@
+import { prisma } from "@/lib/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  const URL = process.env.NEXT_PUBLIC_WEBSITE_URL || "http://localhost:3000/";
+
+  if (!user || user == null || !user.id)
+    throw new Error("something went wrong with authentication" + user);
+
+  let dbUser = await prisma.user.findUnique({
+    where: { id: user.id },
+  });
+
+  if (!dbUser) {
+    dbUser = await prisma.user.create({
+      data: {
+        kindeId: user.id,
+        firstName: user.given_name ?? "",
+        lastName: user.family_name ?? "",
+        email: user.email ?? "",
+      },
+    });
+  }
+
+  return NextResponse.redirect(URL);
+}
