3395e66a1b0b5aebbfbef90f50f0b8138ea2ec3b
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2024-11-21 20:48:55 +0100

    Added simple front-page for testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,30 @@
-export default function Page() {
-  return <></>;
+import { Button } from "@/components/ui/button";
+import {
+  getKindeServerSession,
+  LoginLink,
+  LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/server";
+import Link from "next/link";
+
+export default async function Page() {
+  const { isAuthenticated } = await getKindeServerSession();
+  return (
+    <div className="flex flex-col h-screen justify-center items-center">
+      <h1 className="text-6xl mb-10 font-bold">Sleep tracker</h1>
+      {(await isAuthenticated()) ? (
+        <>
+          <Button asChild size="lg">
+            <LogoutLink>Log out</LogoutLink>
+          </Button>
+          <Button className="mt-4">
+            <Link href="/dashboard">Go to dashboard</Link>
+          </Button>
+        </>
+      ) : (
+        <Button asChild size="lg">
+          <LoginLink>Login</LoginLink>
+        </Button>
+      )}
+    </div>
+  );
 }
