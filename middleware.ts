8cbb445eacb9a9c8031090f602428e08524c637b
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2024-11-22 12:30:58 +0100

    Fix middleware

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,6 @@ import { withAuth } from "@kinde-oss/kinde-auth-nextjs/server";
 export default withAuth(async function middleware(req: any) {}, {
   isReturnToCurrentPage: true,
   loginPage: "/api/auth/login",
-  isAuthorized: true
 });
 
 export const config = {
