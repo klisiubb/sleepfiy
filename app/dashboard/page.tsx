17fc71e6c38c614c2b8960631ffc8bae5bfc1a52
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2024-11-22 12:45:08 +0100

    Dashboard page concept

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,21 @@
+import { SleepCards } from "@/components/sleep-cards";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import React from "react";
 
-export default function Page() {
-  return <div>Page</div>;
+export default async function Page() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  return (
+    <div className="space-y-8">
+      <div className="flex flex-col gap-4">
+        <h1 className="text-4xl font-bold tracking-tight">
+          Hey 👋, {user.given_name}!
+        </h1>
+        <p className="text-muted-foreground">
+          Track and analyze your sleep patterns to improve your rest quality.
+        </p>
+      </div>
+      <SleepCards />
+    </div>
+  );
 }
